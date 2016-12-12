e6c36cbb93a0b2c6c7e8381cbc4e2ebedfa8b097
Zontzip <zontzor@gmail.com>
2016-12-12 22:08:03 +0000

    Refactor user get

--- app.py
+++ app.py
@@ -14,21 +14,42 @@ db = SQLAlchemy(app)
 class Users(db.Model):
     __tablename__ = 'users'
     id = db.Column('id', db.Integer, primary_key=True)
-    username = db.Column('u_username', db.Unicode)
-    password = db.Column('u_password', db.Unicode)
-    email = db.Column('u_email', db.Unicode)
-    firstname = db.Column('u_firstname', db.Unicode)
+    username = db.Column('u_username', db.String(30))
+    password = db.Column('u_password', db.String(30))
+    email = db.Column('u_email', db.String(30))
+    firstname = db.Column('u_firstname', db.String(30))
+    weight = db.Column('u_weight', db.Integer)
+    height = db.Column('u_height', db.Integer)
+    date_created = db.Column('u_date_created', db.Date)
+    profile_image_path = db.Column('u_profile_image_path', db.String(100))
     
     # Users constructor
-    def __init__(self, id, username, password, email, firstname):
+    def __init__(self, id, username, password, email, firstname, weight, height, date_created, profile_image_path):
         self.id = id
         self.username = username
         self.password = password
         self.email = email
         self.firstname = firstname
+        self.weight = weight
+        self.height = height
+        self.date_created = date_created
+        self.profile_image_path = profile_image_path
         
     def __repr__(self):
         return '<Users (%s, %s) >' % (self.username, self.firstname)
+    
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'username' : self.username,
+            'password' : self.password,
+            'email' : self.email,
+            'firstname' : self.firstname,
+            'weight' : self.weight,
+            'height' : self.height,
+            'date_created' : self.date_created,
+            'profile_image_path' : self.profile_image_path
+        }
 
 #############
 # Index
@@ -50,7 +71,7 @@ def get_users():
     data = Users.query.all() #fetch all users on the table
     data_all = []
     for user in data:
-        data_all.append([user.id, user.username, user.email]) #prepare visual data
+        data_all.append(user.serialize()) #prepare visual data
     
     return jsonify(users=data_all)
     
@@ -61,7 +82,7 @@ def get_user(user_id):
     if user is None:
         abort(404)
         
-    result = [user.username, user.password, user.email, user.firstname]
+    result = user.serialize()
     return jsonify(result)
     
 ##################
