c4f22b4fcb3e099603382cdf0c1d1bb3a84452b6
Zontzor <zontzor@gmail.com>
2016-12-19 10:39:37 +0000

    Move app to virtualenv

--- flask/app.py
+++ flask/app.py
@@ -145,4 +145,4 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 500)
 
 if __name__ == "__main__":  
-    app.run(host="0.0.0.0", port=5000, debug = True)
+    app.run(host="0.0.0.0", port=5000)
