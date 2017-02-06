181d65be0b6e7183c8596f8e10b1e05674c319d8
Zontzor <zontzor@gmail.com>
2017-02-06 18:24:00 +0000

    Update venv

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alex/Coding/GlucoseCoach/rest-api/flask"
+set -gx VIRTUAL_ENV "/home/alex/Coding/GlucoseCoach/gc-rest-api/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
