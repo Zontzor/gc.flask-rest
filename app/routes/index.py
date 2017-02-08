bb51acedc3e8f473bc29ef39dac115341411e707
Zontzor <zontzor@gmail.com>
2017-02-08 19:57:25 +0000

    Update index

--- app/routes/index.py
+++ app/routes/index.py
@@ -3,15 +3,20 @@ from app import app
 @app.route("/glucose_coach/api/v1.0")
 def index():  
     return """
+* Denotes authentication required
+ 
 Available API endpoints:
 
-GET /users - List all users
-GET /users/<username> - List a user
+GET /users/<username> - List a user *
 POST /users - Add a user
-PUT /users - Update a users info
-DELETE /users/<username> - Delete a user (Not available atm)
+PUT /users - Update a users info *
+GET /users/usernames/<username> - Get a username
 
-GET /users/<username>/bgreadings - Get a users blood glucose results
-POST /users/<username>/bgreadings - Add a user blood glucose result
-PUT /users/<username>/bgreadings/<bg_id> - Alter a users bg reading
+GET /users/<username>/bgreadings - Get a users blood glucose results *
+POST /users/<username>/bgreadings - Add a user blood glucose result *
+PUT /users/<username>/bgreadings/<bg_id> - Alter a users bg reading *
+
+GET /users/<username>/insdosages - Get a users insulin dosage values *
+POST /users/<username>/insdosages - Add a user insulin dosage value *
+PUT /users/<username>/insdosages/<ins_id> - Alter a users insulin dosage value *
 """ 
