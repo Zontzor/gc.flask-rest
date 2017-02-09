f1adfe44960349a31db9c394faf509491b503414
alex <zontzor@gmail.com>
2017-02-09 11:00:51 +0000

    Add config file, prep for aws

--- app/__init__.py
+++ app/__init__.py
@@ -3,27 +3,25 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_httpauth import HTTPBasicAuth
 
-#import dateutil.parser
-
 app = Flask(__name__)
+
+# Load config settings
+app.config.from_object('config')
+
+# Instantiate db object
+db = SQLAlchemy(app)
 
 auth = HTTPBasicAuth()
 
 # MySQL configurations
-app.config['SQLALCHEMY_DATABASE_URI'] = ("mysql+mysqldb://alex:Cany0n@" + 
-"localhost/glucose_coach")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
-
-# Instantiate db object
-db = SQLAlchemy(app)
+app.config.from_object('config')
 
 import callbacks.authentication
 
 from resources.user import User
 from resources.bgreading import BGReading
 from resources.insdosage import InsDosage
-    
+
 #############
 # Routes
 ############
---
+++ config.py
@@ -0,0 +1,8 @@
+SQLALCHEMY_DATABASE_URI = 'mysql+pymsql://alex:Zontz0_awsmysqldb@gc-mysql-db.chtkdq9vnucp.eu-west-1.rds.amazonaws.com:3306/glucose_coach'
+
+# Uncomment the line below if you want to work with a local DB
+#SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://alex:Cany0n@localhost/glucose_coach'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = 'True'
+
+SECRET_KEY = 'the quick brown fox jumps over the lazy dog'
