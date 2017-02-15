678056a6c57e351f80f25cc099e9db6a0e37e191
alex <zontzor@gmail.com>
2017-02-15 10:45:35 +0000

    Add date created to user

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,16 +1,17 @@
 from app import app, db, auth
-from flask import Flask, jsonify, request, abort, make_response 
+from flask import Flask, jsonify, request, abort, make_response
 from ..resources.user import User
+import datetime
 
 @app.route('/glucose_coach/api/v1.0/users/usernames/<string:user_name>', methods=['GET'])
 def read_username(user_name):
     user = User.query.filter_by(username=user_name).first()
-    
+
     if user is None:
         abort(404)
-        
+
     return jsonify(user.username)
-    
+
 @app.route('/glucose_coach/api/v1.0/users', methods=['GET'])
 @auth.login_required
 def read_users():
@@ -18,17 +19,17 @@ def read_users():
     data_all = []
     for user in data:
         data_all.append(user.serialize()) # prepare visual data
-    
+
     return jsonify(users=data_all)
-    
+
 @app.route('/glucose_coach/api/v1.0/users/<string:user_name>', methods=['GET'])
 @auth.login_required
 def read_user(user_name):
     user = User.query.filter_by(username=user_name).first()
-    
+
     if user is None:
         abort(404)
-        
+
     return jsonify(user.serialize())
 
 @app.route('/glucose_coach/api/v1.0/users', methods=['POST'])
@@ -36,45 +37,49 @@ def create_user():
     username = request.get_json()['username']
     password = request.get_json()['password']
     email = request.get_json()['email']
-    
+
+    date_created = datetime.datetime.now();
+
     if username is None or password is None:
         abort(400) # missing arguments
     if User.query.filter_by(username = username).first() is not None:
         abort(400) # existing user
-        
+
     user = User(username = username, email = email)
-        
+
     user.hash_password(password)
-    
+
+    user.date_created = date_created
+
     curr_session = db.session #open database session
     try:
         curr_session.add(user) #add prepared statment to opened session
         curr_session.commit() #commit changes
     except:
         curr_session.rollback()
-        curr_session.flush() 
+        curr_session.flush()
         abort(400)
-    
+
     return jsonify(user.serialize())
-    
-@app.route('/glucose_coach/api/v1.0/users/<string:user_name>', methods=['PUT']) 
+
+@app.route('/glucose_coach/api/v1.0/users/<string:user_name>', methods=['PUT'])
 @auth.login_required
 def update_user(user_name):
     user = User.query.filter_by(username=user_name).first()
-    
+
     if user is None:
         abort(404)
-    
-    curr_session = db.session 
+
+    curr_session = db.session
     try:
         if 'username' in request.json:
-            user.username = request.get_json()['username'] 
+            user.username = request.get_json()['username']
         if 'password' in request.json:
             user.hash_password(request.get_json()['password'])
         if 'email' in request.json:
-            user.email = request.get_json()['email'] 
+            user.email = request.get_json()['email']
         if 'firstname' in request.json:
-            user.firstname = request.get_json()['firstname'] 
+            user.firstname = request.get_json()['firstname']
         if 'weight' in request.json:
             user.weight = request.get_json()['weight']
         if 'height' in request.json:
@@ -83,7 +88,7 @@ def update_user(user_name):
             user.date_created = request.get_json()['date_created']
         if 'profile_image_path' in request.json:
             user.profile_image_path = request.get_json()['profile_image_path']
-        
+
         curr_session.commit()
     except:
         print("Error")
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-#SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://alex:Zontz0_awsmysqldb@gc-mysql-db.chtkdq9vnucp.eu-west-1.rds.amazonaws.com:3306/glucose_coach'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://alex:Zontz0_awsmysqldb@gc-mysql-db.chtkdq9vnucp.eu-west-1.rds.amazonaws.com:3306/glucose_coach'
 
 # Uncomment the line below if you want to work with a local DB
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://alex:Cany0n@localhost/glucose_coach'
+#SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://alex:Cany0n@localhost/glucose_coach'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = 'True'
 
