ec6310d017171634b0a23e13bbc5cb7f036eef0d
Zontzip <zontzor@gmail.com>
2017-02-16 14:43:23 +0000

    Remove config from project

--- config.py
+++
@@ -1,8 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://alex:Zontz0_aws_rds@glucose-coach.crw4ebbie5fv.eu-west-1.rds.amazonaws.com:3306/glucose_coach'
-
-# Uncomment the line below if you want to work with a local DB
-#SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://alex:Cany0n@localhost/glucose_coach'
-
-SQLALCHEMY_TRACK_MODIFICATIONS = 'True'
-
-SECRET_KEY = 'the quick brown fox jumps over the lazy dog'
