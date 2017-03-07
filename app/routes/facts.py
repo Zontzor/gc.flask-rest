9b5d02d59eafcce27027bf93460aab4482920f77
Zontzip <zontzor@gmail.com>
2017-03-07 23:54:25 +0000

    Add get all facts

--- app/routes/facts.py
+++ app/routes/facts.py
@@ -4,7 +4,7 @@ from ..resources.user import User
 from ..resources.fact import Fact
 
 
-@app.route('/glucose_coach/api/v1.0/facts/<string:user_name>', methods=['GET'])
+@app.route('/glucose_coach/api/v1.0/facts/<string:user_name>/latest', methods=['GET'])
 @auth.login_required
 def read_fact(user_name):
     user = User.query.filter_by(username=user_name).first()
@@ -15,3 +15,20 @@ def read_fact(user_name):
     fact = Fact.query.filter_by(user_id=user.id).order_by(Fact.pf_date.desc(), Fact.pf_time_of_day.desc()).first()
 
     return jsonify(fact.serialize())
+
+
+@app.route('/glucose_coach/api/v1.0/facts/<string:user_name>', methods=['GET'])
+@auth.login_required
+def read_all_facts(user_name):
+    user = User.query.filter_by(username=user_name).first()
+
+    if user is None:
+        abort(404)
+
+    data = Fact.query.filter_by(user_id=user.id).all()
+    data_all = []
+
+    for fact in data:
+        data_all.append(fact.serialize())
+
+    return jsonify(data_all)
